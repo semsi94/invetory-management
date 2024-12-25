e5f4e6e1996c1d987bbe8f39bef8b7392262e5cd
semsi94 <154756937+semsi94@users.noreply.github.com>
2024-12-25 17:25:46 +0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,13 +1,7 @@
-from app.database import db
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    category_id = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"<Item {self.name}>"
+    category_id = db.Column(db.Integer, nullable=True)
