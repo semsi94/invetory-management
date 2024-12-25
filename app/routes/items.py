2a87b6fe3f9147b63b906de3eea64cb3f9f674d8
semsi94 <154756937+semsi94@users.noreply.github.com>
2024-12-25 17:28:55 +0400

    Update items.py

--- app/routes/items.py
+++ app/routes/items.py
@@ -1,17 +1,19 @@
 from flask import Blueprint, request, jsonify
-from app.models import Item
-from app import db
+from app.models import Item, db
 
 # Create a Blueprint object
 items_bp = Blueprint('items', __name__)
 
 # Define the /items route using the Blueprint
-@items_bp.route('/items', methods=['GET'])
+@items_bp.route('/items')
+def items():
+   return "Items home page"
+    
+@items_bp.route('/', methods=['GET'])
 def get_items():
-    items = Item.query.all()  # Ensure database is set up
+    items = Item.query.all()
     return jsonify([{'id': item.id, 'name': item.name, 'quantity': item.quantity, 'category_id': item.category_id} for item in items])
-
-@items_bp.route('/items', methods=['POST'])
+@items_bp.route('/', methods=['POST'])
 def add_item():
     data = request.json
     item = Item(name=data['name'], quantity=data['quantity'], category_id=data['category_id'])
