d96c4e22e89dd22e727d60518abe656392addf0b
semsi94 <154756937+semsi94@users.noreply.github.com>
2024-12-25 17:53:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from app.routes.items import items_bp
 
 app = create_app()
 
-app.register_blueprint(items_bp, url_prefix='/items')
+app.register_blueprint(items_bp) 
 
 @app.route('/')
 def home():
