9a5b9a4d1127abaccaf76e10c47037e5de266edb
semsi94 <154756937+semsi94@users.noreply.github.com>
2024-12-25 16:30:33 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
-from app.routes.items import items_bp  # Import blueprint
+from flask import Flask
+from app.routes.items import items_bp
 
 def create_app():
     app = Flask(__name__)
 
-    
+
     app.register_blueprint(items_bp, url_prefix='/items')
 
-   
+
     @app.route('/')
     def home():
-        return render_template('index.html')  
+        return "Welcome to the Inventory Management System!"
 
     return app
