8bc98edc2f90d72d5fd88d30e40d3860ed819159
semsi94 <154756937+semsi94@users.noreply.github.com>
2024-12-25 15:22:50 +0400

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from app.database import init_db
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    with app.app_context():
+        init_db()
+
+        from app.routes.items import items_bp
+        from app.routes.categories import categories_bp
+
+        app.register_blueprint(items_bp, url_prefix='/items')
+        app.register_blueprint(categories_bp, url_prefix='/categories')
+
+    return app
