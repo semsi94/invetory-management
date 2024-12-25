f99a3f9197762e10dd9614000a72a7c2643cf0b4
semsi94 <154756937+semsi94@users.noreply.github.com>
2024-12-25 17:42:09 +0400

    Update main.py

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ def home():
     return jsonify(data)  # Return JSON data as response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
