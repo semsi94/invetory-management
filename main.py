39a3414a7639542b7093aa3112edb7490f0d4fcc
semsi94 <154756937+semsi94@users.noreply.github.com>
2024-12-25 15:50:02 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
